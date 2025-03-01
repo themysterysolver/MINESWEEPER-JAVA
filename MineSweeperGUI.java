ef4c93e69629fc08b1a8afea3e411f531a913e51
themysterysolver <arjunprabhakar1910@gmail.com>
2025-03-01 08:10:51 +0530

    DEAT:LABL added to NORTH border

--- MineSweeperGUI.java
+++ MineSweeperGUI.java
@@ -12,29 +12,10 @@ public class MineSweeperGUI extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
 
-        JLabel N=new JLabel("NORTH");
-        N.setBackground(new Color(70, 239, 11));
-        add(N,BorderLayout.NORTH);
-        N.setOpaque(true);
-        N.setHorizontalAlignment(SwingConstants.CENTER);
+        JLabel header=new JLabel("Minesweeper");
+        header.setHorizontalAlignment(SwingConstants.CENTER);
+        add(header,BorderLayout.NORTH);
 
-        JLabel E=new JLabel("EAST");
-        E.setBackground(new Color(214, 5, 5));
-        add(E,BorderLayout.EAST);
-        E.setOpaque(true);
-        E.setHorizontalAlignment(SwingConstants.CENTER);
-
-        JLabel W=new JLabel("WEST");
-        W.setBackground(new Color(4, 223, 132));
-        add(W,BorderLayout.WEST);
-        W.setOpaque(true);
-        W.setHorizontalAlignment(SwingConstants.CENTER);
-
-        JLabel S=new JLabel("SOUTH");
-        S.setBackground(new Color(246, 87, 2));
-        add(S,BorderLayout.SOUTH);
-        S.setOpaque(true);
-        S.setHorizontalAlignment(SwingConstants.CENTER);
 
 
         setVisible(true);
