1f857aeac8ff73f45c68fb3f0b9e4943f1e8b765
themysterysolver <arjunprabhakar1910@gmail.com>
2025-03-01 08:26:12 +0530

    FIX:close,center,allignment

--- MineSweeperGUI.java
+++ MineSweeperGUI.java
@@ -9,7 +9,7 @@ public class MineSweeperGUI extends JFrame {
     private void initialize_homepage() {
         setTitle("Minesweeper");
         setSize(400,400);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
 
         JLabel header=new JLabel("Minesweeper");
@@ -18,14 +18,14 @@ public class MineSweeperGUI extends JFrame {
 
         JPanel ceneterPanel=new JPanel();
         ceneterPanel.setLayout(new GridLayout(3,1));
-        JButton easy=new JButton("EASY\n9x9");
-        JButton medium=new JButton("MEDIUM\n16*16");
-        JButton hard=new JButton("HARD\n30x30");
+        JButton easy=new JButton("<html><center>EASY<br>9x9</center></html>");
+        JButton medium=new JButton("<html><center>MEDIUM<br>16x16</center></html>");
+        JButton hard=new JButton("<html><center>HARD<br>30x30</center></html>");
         ceneterPanel.add(easy);
         ceneterPanel.add(medium);
         ceneterPanel.add(hard);
 
-        add(ceneterPanel,SwingConstants.CENTER);
+        add(ceneterPanel,BorderLayout.CENTER);
 
         setLocationRelativeTo(null);
         setVisible(true);
